4641c307d17d1efbf16b00b3ae3d7b0b37ff18b3
Damien Carver <carverdamien@gmail.com>
2019-10-07 16:41:52 +0200

    add list_kernel_installed

--- .lib.sh
+++ .lib.sh
@@ -12,3 +12,22 @@ host_kernel() {
     HOST_KERNEL_SRC="${HOST_KERNEL}/src"
     grep -q "${HOST_KERNEL_SRC}" .gitmodules
 }
+list_kernel_installed() {
+    vmlinuz_pat='/boot/vmlinuz-([0-9]+).([0-9]+).([0-9]+)(.+)-g(.*)'
+    for vmlinuz in /boot/vmlinuz-*
+    do
+	[[ $vmlinuz =~ $vmlinuz_pat ]] || continue
+	version="${BASH_REMATCH[1]}"
+	patchlevel="${BASH_REMATCH[2]}"
+	sublevel="${BASH_REMATCH[3]}"
+	localversion="${BASH_REMATCH[4]}"
+	commit="${BASH_REMATCH[5]}"
+	kernelrelease="${version}.${patchlevel}.${sublevel}${localversion}-g${commit}"
+	[[ -f "/boot/config-${kernelrelease}" ]] || continue
+	[[ -f "/boot/initrd.img-${kernelrelease}" ]] || continue
+	[[ -f "/boot/System.map-${kernelrelease}" ]] || continue
+	[[ -f "/boot/vmlinuz-${kernelrelease}" ]] || continue
+	[[ -d "/lib/modules/${kernelrelease}" ]] || continue
+	echo "${kernelrelease}"
+    done
+}
