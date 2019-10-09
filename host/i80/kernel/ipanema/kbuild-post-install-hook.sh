776edabf605d1fdacff18c98d1c9546f95f298ac
Damien Carver <carverdamien@gmail.com>
2019-10-09 11:35:48 +0200

    kbuild-post-install-hook.sh

--- host/i80/kernel/ipanema/kbuild-post-install-hook.sh
+++ host/i80/kernel/ipanema/kbuild-post-install-hook.sh
@@ -1,1 +1,14 @@
-sudo cp "${HOST_KERNEL_SRC}/tools/sched_monitor/sched_monitor" /usr/bin/
+(
+    sudo cp "${HOST_KERNEL_SRC}/tools/sched_monitor/sched_monitor" /usr/bin/
+    rm -rf "${HOST_KERNEL}/compiler"
+    git submodule init "${HOST_KERNEL}/compiler"
+    git submodule update --recursive --remote "${HOST_KERNEL}/compiler"
+    make -C "${HOST_KERNEL}/compiler" policies
+    export KERNEL=${PWD}/${HOST_KERNEL_SRC}
+    export KERNELRELEASE=${kernelrelease}
+    (
+	cd "${HOST_KERNEL}/compiler/c-code"
+	make
+	make install
+    )
+)
