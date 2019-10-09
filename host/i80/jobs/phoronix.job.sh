c7e20653e5669b524018df90592e6b1f923db721
Damien Carver <carverdamien@gmail.com>
2019-10-09 12:31:31 +0200

    add ipanema to host/i80/jobs/phoronix.job.sh

--- host/i80/jobs/phoronix.job.sh
+++ host/i80/jobs/phoronix.job.sh
@@ -14,38 +14,41 @@ BENCH=bench/phoronix
 PHORONIXES="aobench apache"
 MONITORING=monitoring/all
 MONITORING_SCHEDULED=n
-KERNEL_LOCALVERSION=schedlog
+KERNEL_LOCALVERSIONS="ipanema schedlog"
 SLP=(y         n          )
 GOV=(powersave performance)
 RPT=(1         1          )
-for I in ${!SLP[@]}
+for KERNEL_LOCALVERSION in ${KERNEL_LOCALVERSIONS}
 do
-    SLEEP_STATE=${SLP[$I]}
-    case ${SLEEP_STATE} in
-	y)
-	    CMDLINE=intel_sleep_state_enabled
-	    ;;
-	n)
-	    CMDLINE=intel_sleep_state_disabled
-	    ;;
-	*)
-	    echo '${SLEEP_STATE} must be y|n'
-	    sleep inf
-	    exit 1
-    esac
-    SCALING_GOVERNOR=${GOV[$I]}
-    REPEAT=${RPT[$I]}
-    for N in $(seq ${REPEAT})
+    for I in ${!SLP[@]}
     do
-	for PHORONIX in ${PHORONIXES}
+	SLEEP_STATE=${SLP[$I]}
+	case ${SLEEP_STATE} in
+	    y)
+		CMDLINE=intel_sleep_state_enabled
+		;;
+	    n)
+		CMDLINE=intel_sleep_state_disabled
+		;;
+	    *)
+		echo '${SLEEP_STATE} must be y|n'
+		sleep inf
+		exit 1
+	esac
+	SCALING_GOVERNOR=${GOV[$I]}
+	REPEAT=${RPT[$I]}
+	for N in $(seq ${REPEAT})
 	do
-	    OUTPUT="output/"
-	    OUTPUT+="BENCH=$(basename ${BENCH})/"
-	    OUTPUT+="POWER=${SCALING_GOVERNOR}-${SLEEP_STATE}/"
-	    OUTPUT+="MONITORING=$(basename ${MONITORING})/"
-	    OUTPUT+="PHORONIX=${PHORONIX}/"
-	    OUTPUT+="${KERNEL_LOCALVERSION}/${N}"
-	    run_bench
+	    for PHORONIX in ${PHORONIXES}
+	    do
+		OUTPUT="output/"
+		OUTPUT+="BENCH=$(basename ${BENCH})/"
+		OUTPUT+="POWER=${SCALING_GOVERNOR}-${SLEEP_STATE}/"
+		OUTPUT+="MONITORING=$(basename ${MONITORING})/"
+		OUTPUT+="PHORONIX=${PHORONIX}/"
+		OUTPUT+="${KERNEL_LOCALVERSION}/${N}"
+		run_bench
+	    done
 	done
     done
 done
