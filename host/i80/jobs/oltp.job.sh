5bbc1b81d2abdb596340f3a2fb934ce4f2b978fe
Damien Carver <carverdamien@gmail.com>
2019-10-10 12:16:47 +0200

    host/i80/jobs/oltp.job.sh

---
+++ host/i80/jobs/oltp.job.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+export TIMEOUT
+export IPANEMA_MODULE
+export OUTPUT
+export BENCH
+export MONITORING
+export MONITORING_SCHEDULED
+export MONITORING_START_DELAY
+export MONITORING_STOP_DELAY
+export TASKS
+
+NO_TURBO=0
+TIMEOUT=3600
+IPANEMA_MODULE=
+BENCH=bench/oltp-mysql
+MONITORING=monitoring/all
+MONITORING_SCHEDULED=y
+MONITORING_START_DELAY=60
+MONITORING_STOP_DELAY=10
+KERNEL_LOCALVERSIONS="ipanema"
+SLP=(y         n          )
+GOV=(powersave performance)
+RPT=(1         1          )
+for KERNEL_LOCALVERSION in ${KERNEL_LOCALVERSIONS}
+do
+    for I in ${!SLP[@]}
+    do
+        SLEEP_STATE=${SLP[$I]}
+        case ${SLEEP_STATE} in
+            y)
+                CMDLINE=intel_sleep_state_enabled
+                ;;
+            n)
+                CMDLINE=intel_sleep_state_disabled
+                ;;
+            *)
+                echo '${SLEEP_STATE} must be y|n'
+                sleep inf
+                exit 1
+        esac
+        SCALING_GOVERNOR=${GOV[$I]}
+        REPEAT=${RPT[$I]}
+        for N in $(seq ${REPEAT})
+        do
+            for TASKS in 160
+            do
+            	for IPANEMA_MODULE in 12300
+            	do
+                	OUTPUT="output/"
+			   		OUTPUT+="BENCH=$(basename ${BENCH})/"
+				   	OUTPUT+="POWER=${SCALING_GOVERNOR}-${SLEEP_STATE}/"
+				   	OUTPUT+="MONITORING=$(basename ${MONITORING})/"
+				   	OUTPUT+="IPANEMA=$(basename ${IPANEMA_MODULE})/"
+				   	OUTPUT+="${TASKS}-${KERNEL_LOCALVERSIONS}/${N}"
+	                run_bench
+	            done
+            done
+        done
+    done
+done
