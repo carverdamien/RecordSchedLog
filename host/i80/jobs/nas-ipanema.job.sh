cec73a6228b940e94658d6dd5e72d8363c1d87c1
Jean-Pierre Lozi <jean-pierre@lozi.org>
2019-10-13 17:15:48 +0200

    Add NAS benchmark scripts

---
+++ host/i80/jobs/nas-ipanema.job.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+export TIMEOUT
+export IPANEMA_MODULE
+export OUTPUT
+export BENCH
+export BENCH_NAME
+export BENCH_CLASS
+export MONITORING
+export MONITORING_SCHEDULED
+export MONITORING_START_DELAY
+export MONITORING_STOP_DELAY
+export TASKS
+
+NO_TURBO=0
+TIMEOUT=3600
+IPANEMA_MODULE=
+BENCH=bench/nas
+BENCH_NAMES=(   bt cg dc ep ft    lu mg sp sp ua ua ) # is
+BENCH_CLASSES=( B  C  A  C  C     B  D  A  B  B  C )  # D
+MONITORING=monitoring/all
+MONITORING_SCHEDULED=y
+MONITORING_START_DELAY=60
+MONITORING_STOP_DELAY=10
+KERNEL_LOCALVERSIONS="ipanema"
+IPANEMA_MODULES="44530 12300 10261 cfs_wwc"
+SLP=(n           y        )
+GOV=(performance powersave)
+RPT=(1         1          )
+for KERNEL_LOCALVERSION in ${KERNEL_LOCALVERSIONS}
+do
+    for I in ${!SLP[@]}
+    do
+        SLEEP_STATE=${SLP[$I]}
+        case ${SLEEP_STATE} in
+            y)
+                CMDLINE=intel_sleep_state_enabled
+                ;;
+            n)
+                CMDLINE=intel_sleep_state_disabled
+                ;;
+            *)
+                echo '${SLEEP_STATE} must be y|n'
+                sleep inf
+                exit 1
+        esac
+        SCALING_GOVERNOR=${GOV[$I]}
+        REPEAT=${RPT[$I]}
+        for N in $(seq ${REPEAT})
+        do
+            for I in ${!BENCH_NAMES[@]}
+            do
+                for TASKS in 80 160
+                do
+                    for IPANEMA_MODULE in ${IPANEMA_MODULES}
+                    do
+                        BENCH_NAME=${BENCH_NAMES[$I]}
+                        BENCH_CLASS=${BENCH_CLASSES[$I]}
+
+                        OUTPUT="output/"
+                        OUTPUT+="BENCH=$(basename ${BENCH})_${BENCH_NAME}.${BENCH_CLASS}/"
+                        OUTPUT+="POWER=${SCALING_GOVERNOR}-${SLEEP_STATE}/"
+                        OUTPUT+="MONITORING=$(basename ${MONITORING})/"
+                        OUTPUT+="IPANEMA=$(basename ${IPANEMA_MODULE})/"
+                        OUTPUT+="${TASKS}-${KERNEL_LOCALVERSION}/${N}"
+
+                        run_bench
+                    done
+                done
+            done
+        done
+    done
+done
+
