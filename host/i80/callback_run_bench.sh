3b305c9816f2e89bebaf2590e232c403b69d06c4
Damien Carver <carverdamien@gmail.com>
2019-11-01 17:30:21 +0100

    disable rsync

--- host/i80/callback_run_bench.sh
+++ host/i80/callback_run_bench.sh
@@ -5,7 +5,7 @@ SRC_DIR=/mnt/data/damien/git/carverdamien/RecordSchedLog/output/
 DSTS="damien@amd48b-systeme.rsr.lip6.fr:/mnt/data/damien/git/carverdamien/SchedDisplay/examples/trace/ damien@i44.rsr.lip6.fr:/mnt/data/damien/git/carverdamien/SchedDisplay/examples/trace/"
 for DST in ${DSTS}
 do
-    rsync -e "ssh -i /home/damien/.ssh/id_rsa" --size-only -rvP "${SRC_DIR}" "${DST}"
+    echo rsync -e "ssh -i /home/damien/.ssh/id_rsa" --size-only -rvP "${SRC_DIR}" "${DST}"
 done
 if [ -x ./host/${HOSTNAME}/notify ]
 then
