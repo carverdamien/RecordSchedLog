d8803ebc7335831cba11eee74abc2023e4ed0774
Damien Carver <carverdamien@gmail.com>
2019-11-18 10:12:50 +0100

    .

--- host/i80/jobs/010/kbuild.job.sh
+++ host/i80/jobs/010/kbuild.job.sh
@@ -17,9 +17,9 @@ MONITORING=monitoring/all
 MONITORING_SCHEDULED=n
 KERNEL_LOCALVERSIONS="ipanema pull-back" # "no-preempt-wakeup pull-back sched-freq local local-light ipanema"
 KERNEL_COMMIT=54ecb8f7028c5eb3d740bb82b0f1d90f2df63c5c
-SLP=(n          ) # y
-GOV=(performance) # powersave
-RPT=(2          ) # 1
+SLP=(n           y)
+GOV=(performance powersave)
+RPT=(1           2)
 for KERNEL_LOCALVERSION in ${KERNEL_LOCALVERSIONS}
 do
     for I in ${!SLP[@]}
