65cf13f22a1c8a1a366897001f71ca6ba501a84f
Damien Carver <carverdamien@gmail.com>
2019-11-29 14:14:56 +0100

    scripts/heatmap.py

--- scripts/heatmap.py
+++ scripts/heatmap.py
@@ -10,37 +10,76 @@ sns.set()
 
 def main():
     # ./scripts/heatmap.py out.pdf in.csv energy|usr_bin_time
-    _, output_file, input_file, column_name = sys.argv
-    print(output_file, input_file)
+    _, output_file, input_file, column_name, agg = sys.argv
+    print(output_file, input_file, column_name, agg)
+    agg = {
+        'min'  : np.min,
+        'max'  : np.max,
+        'mean' : np.mean,
+    }[agg]
     raw = pd.read_csv(input_file, sep=';')
     raw[column_name] = raw[column_name].astype(float)
     print(raw)
+    HOST='i80'
+    POWER='powersave-y'
+    MONITORING='cpu-energy-meter'
     Y = [
         {
-            'index' : f"kbuild-{target}-{tasks}",
-            'template' : Template(f"/mnt/data/damien/git/carverdamien/SchedDisplay/examples/trace/BENCH=kbuild-{target}/POWER=powersave-y/MONITORING=all/{tasks}-$kernel/.*tar"),
-            'match' : lambda y, kernel    : y['template'].substitute(kernel=kernel),
-            'perf'  : lambda y, match     : np.mean(raw[raw['fname'].str.match(match)][column_name]),
+            'index' : f"{bench}-{tasks}",
+            'template' : Template(f"/mnt/data/damien/git/carverdamien/SchedDisplay/examples/trace/HOST={HOST}/BENCH={bench}/POWER={POWER}/MONITORING={MONITORING}/LP=$lp/{tasks}-$kernel/.*.tar"),
+            'match' : lambda y, x    : y['template'].substitute(**x['sub']),
+            'perf'  : lambda y, match     : agg(raw[raw['fname'].str.match(match)][column_name]),
             'norm'  : lambda y, perf, ref : 100.0*(ref-perf)/ref,
         }
-        for target in ['all','sched']
+        for bench in ['kbuild-all','kbuild-sched']
         for tasks in ['80','160','320']
     ] + [
         {
             'index' : f"{bench}-{tasks}",
-            'template' : Template(f"/mnt/data/damien/git/carverdamien/SchedDisplay/examples/trace/BENCH={bench}/POWER=powersave-y/MONITORING=all/{tasks}-$kernel/.*tar"),
-            'match' : lambda y, kernel    : y['template'].substitute(kernel=kernel),
-            'perf'  : lambda y, match     : np.mean(raw[raw['fname'].str.match(match)][column_name]),
+            'template' : Template(f"/mnt/data/damien/git/carverdamien/SchedDisplay/examples/trace/HOST={HOST}/BENCH={bench}/POWER={POWER}/MONITORING={MONITORING}/LP=$lp/{tasks}-$kernel/.*.tar"),
+            'match' : lambda y, x    : y['template'].substitute(**x['sub']),
+            'perf'  : lambda y, match     : agg(raw[raw['fname'].str.match(match)][column_name]),
             'norm'  : lambda y, perf, ref : 100.0*(ref-perf)/ref,
         }
         for bench in ['nas_bt.B', 'nas_cg.C', 'nas_ep.C', 'nas_ft.C', 'nas_lu.B', 'nas_sp.B', 'nas_ua.B']
         for tasks in ['80','160']
     ]
-    X = ['ipanema', 'local', 'local-light', 'pull-back', 'sched-freq']
-    XREF = X[0]
+    X = [
+        {
+            'index' : '4.19',
+            'sub' : {'kernel':'ipanema','lp':'n'},
+        },
+        {
+            'index' : '5.4',
+            'sub' : {'kernel':'schedlog','lp':'n'},
+        },
+        {
+            'index' : 'local',
+            'sub' : {'kernel':'local','lp':'n'},
+        },
+        {
+            'index' : 'local-light',
+            'sub' : {'kernel':'local-light','lp':'n'},
+        },
+        {
+            'index' : 'lp-0',
+            'sub' : {'kernel':'lp','lp':'0'},
+        },
+        {
+            'index' : 'lp-1',
+            'sub' : {'kernel':'lp','lp':'1'},
+        },
+        {
+            'index' : 'lp-2',
+            'sub' : {'kernel':'lp','lp':'2'},
+        },
+    ]
+    XREF = X[1]
+    i=6
+    print(Y[i]['match'](Y[i],X[0]))
     CELL = lambda x,y : y['norm'](y, y['perf'](y, y['match'](y, x)),y['perf'](y, y['match'](y, XREF)))
     data = [[CELL(x,y) for x in X] for y in Y]
-    df = pd.DataFrame(data, columns=X, index=[y['index'] for y in Y])
+    df = pd.DataFrame(data, columns=[x['index'] for x in X], index=[y['index'] for y in Y])
     print(df)
     vmax = df.max().max()
     vmin = -vmax
