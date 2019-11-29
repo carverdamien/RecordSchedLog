faa94f98b9a364a248ab08a877c1ca42e16fc34c
Damien Carver <carverdamien@gmail.com>
2019-11-29 17:48:14 +0100

    llvm-build.job.sh

---
+++ host/i80/jobs/100/llvm-build.job.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+export TIMEOUT
+export IPANEMA_MODULE
+export OUTPUT
+export BENCH
+export MONITORING
+export MONITORING_SCHEDULED
+export SYSCTL=''
+
+NO_TURBO=0
+TIMEOUT=3600
+IPANEMA_MODULE=
+BENCH=bench/llvmcmake
+MONITORING_SCHEDULED=n
+KERNEL_LOCALVERSIONS=(lp lp lp schedlog local local-light ipanema)
+LP_VALUES=(1 2 0 n n n n)
+SLP=(y y)
+GOV=(powersave powersave)
+RPT=(1 10)
+MON=(monitoring/all monitoring/cpu-energy-meter)
+
+for J in ${!KERNEL_LOCALVERSIONS[@]}
+do
+    KERNEL_LOCALVERSION=${KERNEL_LOCALVERSIONS[$J]}
+    LP_VALUE=${LP_VALUES[$J]}
+        case ${LP_VALUE} in
+	n)
+	    SYSCTL=''
+	    ;;
+	*)
+	    SYSCTL="kernel.sched_local_placement=${LP_VALUE}"
+	    ;;
+    esac
+    for I in ${!SLP[@]}
+    do
+	SLEEP_STATE=${SLP[$I]}
+	case ${SLEEP_STATE} in
+	    y)
+		CMDLINE=intel_sleep_state_enabled
+		;;
+	    n)
+		CMDLINE=intel_sleep_state_disabled
+		;;
+	    *)
+		echo '${SLEEP_STATE} must be y|n'
+		sleep inf
+		exit 1
+	esac
+	SCALING_GOVERNOR=${GOV[$I]}
+	REPEAT=${RPT[$I]}
+	MONITORING=${MON[$I]}
+	for N in $(seq ${REPEAT})
+	do
+	    OUTPUT="output/"
+	    OUTPUT+="HOST=${HOSTNAME}/"
+	    OUTPUT+="BENCH=$(basename ${BENCH})/"
+	    OUTPUT+="POWER=${SCALING_GOVERNOR}-${SLEEP_STATE}/"
+	    OUTPUT+="MONITORING=$(basename ${MONITORING})/"
+	    OUTPUT+="LP=${LP_VALUE}/"
+	    OUTPUT+="${KERNEL_LOCALVERSION}/${N}"
+	    run_bench
+	done
+    done
+done
