97474b6d61d8d34e2188541317a42120ce1d55e4
Damien Carver <carverdamien@gmail.com>
2019-12-05 11:37:36 +0100

    add flock

--- scripts/heatmap.sh
+++ scripts/heatmap.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 # Runs on i44 (cron every minutes)
-exec 1>/home/damien/git/carverdamien/RecordSchedLog/scripts/heatmap.stdout
-exec 2>/home/damien/git/carverdamien/RecordSchedLog/scripts/heatmap.stderr
-cd /home/damien/git/carverdamien/RecordSchedLog
-
-set -e -x -u
-date
-
-./scripts/tar2raw.sh master /mnt/data/damien/git/carverdamien/SchedDisplay/examples/trace/HOST=i80 i80.csv
-./scripts/filter_cpu_energy_meter.py filter_cpu_energy_meter.csv i80.csv
-sed 's/.mnt.data.damien.git.carverdamien.SchedDisplay.examples.trace/output/' filter_cpu_energy_meter.csv | xargs shasum > host/i80/discard
-
-for value in perf energy
-do
-    for agg in min max mean median std
+main() {
+    exec 1>/home/damien/git/carverdamien/RecordSchedLog/scripts/heatmap.stdout
+    exec 2>/home/damien/git/carverdamien/RecordSchedLog/scripts/heatmap.stderr
+    cd /home/damien/git/carverdamien/RecordSchedLog
+    
+    set -e -x -u
+    date
+    
+    ./scripts/tar2raw.sh master /mnt/data/damien/git/carverdamien/SchedDisplay/examples/trace/HOST=i80 i80.csv
+    ./scripts/filter_cpu_energy_meter.py filter_cpu_energy_meter.csv i80.csv
+    sed 's/.mnt.data.damien.git.carverdamien.SchedDisplay.examples.trace/output/' filter_cpu_energy_meter.csv | xargs shasum > host/i80/discard
+    rm -f $(cat filter_cpu_energy_meter.csv)
+    
+    for value in perf energy
     do
-	for n in normed raw
+	for agg in min max mean median std
 	do
-	    ./scripts/heatmap.py "heatmaps/$n.$value.$agg.pdf" i80.csv $value $agg $n
+	    for n in normed raw
+	    do
+		./scripts/heatmap.py "heatmaps/$n.$value.$agg.pdf" i80.csv $value $agg $n
+	    done
 	done
     done
-done
+}
+
+(
+    flock -n 9 || exit 1
+    main
+) 9>/tmp/heatmap.lock
