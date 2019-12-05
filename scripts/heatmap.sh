fd096712d6adefd358da334a04bad7937521c32c
Damien Carver <carverdamien@gmail.com>
2019-12-05 11:33:25 +0100

    generate host/i80/discard

--- scripts/heatmap.sh
+++ scripts/heatmap.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+# Runs on i44 (cron every minutes)
+exec 1>/home/damien/git/carverdamien/RecordSchedLog/scripts/heatmap.stdout
+exec 2>/home/damien/git/carverdamien/RecordSchedLog/scripts/heatmap.stderr
+cd /home/damien/git/carverdamien/RecordSchedLog
+
 set -e -x -u
+date
 
 ./scripts/tar2raw.sh master /mnt/data/damien/git/carverdamien/SchedDisplay/examples/trace/HOST=i80 i80.csv
+./scripts/filter_cpu_energy_meter.py filter_cpu_energy_meter.csv i80.csv
+sed 's/.mnt.data.damien.git.carverdamien.SchedDisplay.examples.trace/output/' filter_cpu_energy_meter.csv | xargs shasum > host/i80/discard
 
 for value in perf energy
 do
