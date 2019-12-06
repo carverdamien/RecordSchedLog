66616df108a6b5f9ca61c34f963d18eefdc8219a
Damien Carver <carverdamien@gmail.com>
2019-12-06 17:56:55 +0100

    300/build-mysql-server.job.sh

---
+++ host/latitude/jobs/300/build-mysql-server.job.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+export TIMEOUT
+export IPANEMA_MODULE
+export OUTPUT
+export BENCH
+export MONITORING
+export MONITORING_SCHEDULED
+export TASKS
+export SYSCTL=''
+
+NO_TURBO=0
+TIMEOUT=3600
+IPANEMA_MODULE=
+BENCH=bench/build-mysql-server
+MONITORING_SCHEDULED=n
+KERNEL_LOCALVERSIONS=(delayed-placement)
+LP_VALUES=(n)
+SLP=(y)
+GOV=(powersave)
+RPT=(1)
+MON=(monitoring/cpu-energy-meter)
+
+for J in ${!KERNEL_LOCALVERSIONS[@]}
+do
+    KERNEL_LOCALVERSION=${KERNEL_LOCALVERSIONS[$J]}
+    LP_VALUE=${LP_VALUES[$J]}
+    case ${LP_VALUE} in
+	n)
+	    SYSCTL=''
+	    ;;
+	*)
+	    SYSCTL="kernel.sched_local_placement=${LP_VALUE}"
+	    ;;
+    esac
+    for I in ${!SLP[@]}
+    do
+	SLEEP_STATE=${SLP[$I]}
+	case ${SLEEP_STATE} in
+	    y)
+		CMDLINE=intel_sleep_state_enabled
+		;;
+	    n)
+		CMDLINE=intel_sleep_state_disabled
+		;;
+	    *)
+		echo '${SLEEP_STATE} must be y|n'
+		sleep inf
+		exit 1
+	esac
+	SCALING_GOVERNOR=${GOV[$I]}
+	REPEAT=${RPT[$I]}
+	MONITORING=${MON[$I]}
+	for N in $(seq ${REPEAT})
+	do
+	    for TASKS in 4 8 16
+	    do
+		OUTPUT="output/"
+		OUTPUT+="HOST=${HOSTNAME}/"
+		OUTPUT+="BENCH=$(basename ${BENCH})/"
+		OUTPUT+="POWER=${SCALING_GOVERNOR}-${SLEEP_STATE}/"
+		OUTPUT+="MONITORING=$(basename ${MONITORING})/"
+		OUTPUT+="LP=${LP_VALUE}/"
+		OUTPUT+="${TASKS}-${KERNEL_LOCALVERSION}/${N}"
+		run_bench
+	    done
+	done
+    done
+done
