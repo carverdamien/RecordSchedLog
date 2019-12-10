06a7e67fd417a972b3fc570ace25e604a3a01d5c
Redha Gouicem <redha.gouicem@gmail.com>
2019-12-10 15:25:56 +0100

    host: redha: rsync to correct dir

--- host/redha/callback_run_bench.sh
+++ host/redha/callback_run_bench.sh
@@ -2,7 +2,7 @@
 set -e -x -u
 TAR="$1"
 SRC_DIR=/home/redha/RecordSchedLog/output/
-DSTS="redha@i44.rsr.lip6.fr:/mnt/data/damien/git/carverdamien/SchedDisplay/examples/trace/redha/"
+DSTS="redha@i44.rsr.lip6.fr:/mnt/data/damien/git/carverdamien/SchedDisplay/examples/trace/"
 for DST in ${DSTS}
 do
     rsync -e "ssh -i /home/redha/.ssh/id_rsa" --size-only -rvP "${SRC_DIR}" "${DST}"
