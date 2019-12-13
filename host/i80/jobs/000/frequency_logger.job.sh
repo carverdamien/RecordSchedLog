71c4e220fed0d58c1498a84b96770c1e93be89f1
Damien Carver <carverdamien@gmail.com>
2019-12-13 10:25:37 +0100

    host/i80/jobs/000/frequency_logger.job.sh

--- host/i80/jobs/000/frequency_logger.job.sh
+++ host/i80/jobs/000/frequency_logger.job.sh
@@ -14,38 +14,53 @@ TIMEOUT=3600
 IPANEMA_MODULE=
 BENCH=bench/frequency_logger
 MONITORING_SCHEDULED=n
-KERNEL_LOCALVERSION=local-cpuofwaker # any kernel
+KERNEL_LOCALVERSIONS=(local-cpuofwaker) # local-light-cpuofwaker lp lp lp schedlog local local-light ipanema delayed-placement)
+LP_VALUES=(n) # n 1 2 0 n n n n n)
 SLP=(y n)
 GOV=(powersave performance)
 RPT=(1 1)
 MON=(monitoring/nop monitoring/nop monitoring/nop)
 
-for I in ${!SLP[@]}
+for J in ${!KERNEL_LOCALVERSIONS[@]}
 do
-    SLEEP_STATE=${SLP[$I]}
-    case ${SLEEP_STATE} in
-	y)
-	    CMDLINE=intel_sleep_state_enabled
-		;;
+    KERNEL_LOCALVERSION=${KERNEL_LOCALVERSIONS[$J]}
+    LP_VALUE=${LP_VALUES[$J]}
+    case ${LP_VALUE} in
 	n)
-	    CMDLINE=intel_sleep_state_disabled
+	    SYSCTL=''
 	    ;;
 	*)
-	    echo '${SLEEP_STATE} must be y|n'
-	    sleep inf
-		exit 1
+	    SYSCTL="kernel.sched_local_placement=${LP_VALUE}"
+	    ;;
     esac
-    SCALING_GOVERNOR=${GOV[$I]}
-    REPEAT=${RPT[$I]}
-    MONITORING=${MON[$I]}
-    for N in $(seq ${REPEAT})
+    for I in ${!SLP[@]}
     do
-	OUTPUT="output/"
-	OUTPUT+="HOST=${HOSTNAME}/"
-	OUTPUT+="BENCH=$(basename ${BENCH})/"
-	OUTPUT+="POWER=${SCALING_GOVERNOR}-${SLEEP_STATE}/"
-	OUTPUT+="MONITORING=$(basename ${MONITORING})/"
-	OUTPUT+="${KERNEL_LOCALVERSION}/${N}"
-	run_bench
+	SLEEP_STATE=${SLP[$I]}
+	case ${SLEEP_STATE} in
+	    y)
+		CMDLINE=intel_sleep_state_enabled
+		;;
+	    n)
+		CMDLINE=intel_sleep_state_disabled
+		;;
+	    *)
+		echo '${SLEEP_STATE} must be y|n'
+	    sleep inf
+	    exit 1
+	esac
+	SCALING_GOVERNOR=${GOV[$I]}
+	REPEAT=${RPT[$I]}
+	MONITORING=${MON[$I]}
+	for N in $(seq ${REPEAT})
+	do
+	    OUTPUT="output/"
+	    OUTPUT+="HOST=${HOSTNAME}/"
+	    OUTPUT+="BENCH=$(basename ${BENCH})/"
+	    OUTPUT+="POWER=${SCALING_GOVERNOR}-${SLEEP_STATE}/"
+	    OUTPUT+="MONITORING=$(basename ${MONITORING})/"
+	    OUTPUT+="LP=${LP_VALUE}/"
+	    OUTPUT+="${KERNEL_LOCALVERSION}/${N}"
+	    run_bench
+	done
     done
 done
