f05f5397c222c44f8a04762b6c80da5d61daf85c
Damien Carver <carverdamien@gmail.com>
2020-01-07 14:06:13 +0100

    001-damien.job.sh

---
+++ host/i80/jobs/000/001-damien.job.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+export TIMEOUT
+export IPANEMA_MODULE
+export OUTPUT
+export BENCH
+export MONITORING
+export MONITORING_SCHEDULED
+export TASKS
+export TARGET
+export SYSCTL=''
+
+NO_TURBO=0
+TIMEOUT=3600
+IPANEMA_MODULE=
+BENCH=bench/kbuild
+MONITORING_SCHEDULED=n
+KERNEL_LOCALVERSIONS=(5.4-delayed-placement-onoff)
+LP_VALUES=(n)
+SLP=(y)
+GOV=(powersave)
+RPT=(1)
+MON=(monitoring/all)
+
+for J in ${!KERNEL_LOCALVERSIONS[@]}
+do
+    KERNEL_LOCALVERSION=${KERNEL_LOCALVERSIONS[$J]}
+    LP_VALUE=${LP_VALUES[$J]}
+    case ${LP_VALUE} in
+	n)
+	    SYSCTL=''
+	    ;;
+	*)
+	    if [ ${KERNEL_LOCALVERSION} == "delayed-placement" ] ; then
+		SYSCTL="kernel.sched_delayed_placement=${LP_VALUE}"
+	    else
+		SYSCTL="kernel.sched_local_placement=${LP_VALUE}"
+	    fi
+	    ;;
+    esac
+    for I in ${!SLP[@]}
+    do
+	SLEEP_STATE=${SLP[$I]}
+	SCALING_GOVERNOR=${GOV[$I]}
+	REPEAT=${RPT[$I]}
+	MONITORING=${MON[$I]}
+	case ${SLEEP_STATE} in
+	    y)
+		case ${SCALING_GOVERNOR} in
+		    schedutil)
+			CMDLINE=intel_sleep_state_enabled_pstate_passive
+			;;
+		    *)
+			CMDLINE=intel_sleep_state_enabled
+			;;
+		esac
+		;;
+	    n)
+		CMDLINE=intel_sleep_state_disabled
+		;;
+	    *)
+		echo '${SLEEP_STATE} must be y|n'
+		sleep inf
+		exit 1
+	esac
+	for N in $(seq ${REPEAT})
+	do
+	    for TASKS in 80 160 320 512 1024
+	    do
+		for TARGET in all kernel/sched/
+		do
+		    OUTPUT="output/"
+		    OUTPUT+="HOST=${HOSTNAME}/"
+		    OUTPUT+="BENCH=$(basename ${BENCH})-$(basename ${TARGET})/"
+		    OUTPUT+="POWER=${SCALING_GOVERNOR}-${SLEEP_STATE}/"
+		    OUTPUT+="MONITORING=$(basename ${MONITORING})/"
+		    OUTPUT+="LP=${LP_VALUE}/"
+		    OUTPUT+="${TASKS}-${KERNEL_LOCALVERSION}/${N}"
+		    run_bench
+		done
+	    done
+	done
+    done
+done
