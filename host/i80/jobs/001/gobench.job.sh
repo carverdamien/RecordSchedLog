dddc397978ea3f9e6588a8e286f97df0b49a65ed
Damien Carver <carverdamien@gmail.com>
2020-01-09 12:13:59 +0100

    gobench.job.sh

---
+++ host/i80/jobs/001/gobench.job.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+export TIMEOUT
+export IPANEMA_MODULE
+export OUTPUT
+export BENCH
+export MONITORING
+export MONITORING_SCHEDULED
+export SYSCTL=''
+export PHORONIX PHORONIX_TEST_ARGUMENTS
+
+NO_TURBO=0
+TIMEOUT=3600
+IPANEMA_MODULE=
+BENCH=bench/phoronix
+PHORONIXES=(go-benchmark go-benchmark go-benchmark go-benchmark)
+PARGUMENTS=(1 2 3 4)
+MONITORING_SCHEDULED=n
+KERNEL_LOCALVERSIONS=(5.4-fdp schedlog)
+LP_VALUES=(n n)
+SLP=(y)
+GOV=(powersave)
+RPT=(10)
+MON=(monitoring/all)
+
+for J in ${!KERNEL_LOCALVERSIONS[@]}
+do
+    KERNEL_LOCALVERSION=${KERNEL_LOCALVERSIONS[$J]}
+    LP_VALUE=${LP_VALUES[$J]}
+    case ${LP_VALUE} in
+	n)
+	    SYSCTL=''
+	    ;;
+	*)
+	    if [ ${KERNEL_LOCALVERSION} == "delayed-placement" ] ; then
+		SYSCTL="kernel.sched_delayed_placement=${LP_VALUE}"
+	    else
+		SYSCTL="kernel.sched_local_placement=${LP_VALUE}"
+	    fi
+	    ;;
+    esac
+    for I in ${!SLP[@]}
+    do
+	SLEEP_STATE=${SLP[$I]}
+	SCALING_GOVERNOR=${GOV[$I]}
+	REPEAT=${RPT[$I]}
+	MONITORING=${MON[$I]}
+	case ${SLEEP_STATE} in
+	    y)
+		case ${SCALING_GOVERNOR} in
+		    schedutil)
+			CMDLINE=intel_sleep_state_enabled_pstate_passive
+			;;
+		    *)
+			CMDLINE=intel_sleep_state_enabled
+			;;
+		esac
+		;;
+	    n)
+		CMDLINE=intel_sleep_state_disabled
+		;;
+	    *)
+		echo '${SLEEP_STATE} must be y|n'
+		sleep inf
+		exit 1
+	esac
+	for N in $(seq ${REPEAT})
+	do
+	    for K in ${!PHORONIXES[@]}
+	    do
+		PHORONIX=${PHORONIXES[$K]}
+		PHORONIX_TEST_ARGUMENTS=${PARGUMENTS[$K]}
+		OUTPUT="output/"
+		OUTPUT+="HOST=${HOSTNAME}/"
+		OUTPUT+="BENCH=$(basename ${BENCH})/"
+		OUTPUT+="POWER=${SCALING_GOVERNOR}-${SLEEP_STATE}/"
+		OUTPUT+="MONITORING=$(basename ${MONITORING})/"
+		OUTPUT+="LP=${LP_VALUE}/"
+		OUTPUT+="PHORONIX=${PHORONIX}-${PHORONIX_TEST_ARGUMENTS}/"
+		OUTPUT+="${KERNEL_LOCALVERSION}/${N}"
+		run_bench
+	    done
+	done
+    done
+done
