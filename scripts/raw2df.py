8b25fbaa9ce97d618944b610d9d1bf0c0c266e81
Redha Gouicem <redha.gouicem@gmail.com>
2020-01-11 17:12:33 +0100

    scripts: raw2df: parse fdp-percpu kernel

--- scripts/raw2df.py
+++ scripts/raw2df.py
@@ -74,6 +74,8 @@ for d in in_data.iterrows():
         sched = 'fdp-{}'.format(50 if lp == 'n' else lp)
     elif sched in [ '5.4-fdp-nom' ]:
         sched = 'fdp-nom-{}'.format(50 if lp == 'n' else lp)
+    elif sched in [ '5.4-fdp-percpu' ]:
+        sched = 'fdp-percpu-{}'.format(50 if lp == 'n' else lp)
     elif sched in [ 'local-placement', 'lp' ]:
         sched = 'lp-{}'.format(lp)
     elif sched == 'local':
