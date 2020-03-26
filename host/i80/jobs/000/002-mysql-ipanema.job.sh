f3633f4d38431ca248413c3f47e6d0008fafc350
Damien Carver <carverdamien@gmail.com>
2020-03-26 10:33:00 +0100

    cp 002-kbuild-ipanema.job.sh 002-mysql-ipanema.job.sh

---
+++ host/i80/jobs/000/002-mysql-ipanema.job.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+export TIMEOUT
+export IPANEMA_MODULE
+export OUTPUT
+export BENCH
+export BENCH_NAME
+export BENCH_CLASS
+export MONITORING
+export MONITORING_SCHEDULED
+export MONITORING_START_DELAY
+export MONITORING_STOP_DELAY
+export TASKS
+export TARGET
+export SYSCTL=''
+
+export PERF_STAT_OPT='-e mem_load_uops_l3_miss_retired.local_dram -e mem_load_uops_l3_miss_retired.remote_dram'
+export TRACECMD_RECORD_OPT="-e sched -b $((2**20))"
+
+NO_TURBO=0
+TIMEOUT=3600
+IPANEMA_MODULE=
+BENCH=bench/kbuild
+TARGET=all
+MONITORING_SCHEDULED=n
+
+KERNEL_LOCALVERSION=ipanema-eurosys2020
+SLEEP_STATE=n
+SCALING_GOVERNOR=performance
+
+IPANEMA_MODULES=()
+REPEATS=()
+MONITORINGS=()
+
+# # trace-cmd first
+# for ipa in '' cfs_wwc ule_wwc
+# do
+#     IPANEMA_MODULES+=("$ipa")
+#     REPEATS+=(1)
+#     MONITORINGS+=(monitoring/trace-cmd)
+# done
+# SchedLog first
+# for ipa in cfs_wwc # '' cfs_wwc_flat ule_wwc ule
+# do
+#    IPANEMA_MODULES+=("$ipa")
+#    REPEATS+=(6)
+#    MONITORINGS+=(monitoring/SchedLog)
+# done
+# # perf_stat
+# for ipa in '' cfs_wwc ule_wwc ule_wwc_rip ule_rip ule
+# do
+#     IPANEMA_MODULES+=("$ipa")
+#     REPEATS+=(10)
+#     MONITORINGS+=(monitoring/perf_stat)
+# done
+# nop last
+for ipa in '' ule_wwc_2 cfs_wwc ule cfs_wwc_flat # ule_wwc cfs_cwc_ipa cfs_cwc_flat_ipa ule_ipa ule_wwc_rip ule_rip
+do
+     IPANEMA_MODULES+=("$ipa")
+     REPEATS+=($MAX_RPT)
+     MONITORINGS+=(monitoring/nop)
+done
+
+for J in ${!IPANEMA_MODULES[@]}
+do
+    IPANEMA_MODULE=${IPANEMA_MODULES[$J]}
+    REPEAT=${REPEATS[$J]}
+    MONITORING=${MONITORINGS[$J]}
+    case ${SLEEP_STATE} in
+	y)
+	    case ${SCALING_GOVERNOR} in
+		schedutil)
+		    CMDLINE=intel_sleep_state_enabled_pstate_passive
+		    ;;
+		*)
+		    CMDLINE=intel_sleep_state_enabled
+		    ;;
+	    esac
+	    ;;
+	n)
+	    CMDLINE=intel_sleep_state_disabled
+	    ;;
+	*)
+		echo '${SLEEP_STATE} must be y|n'
+		sleep inf
+		exit 1
+    esac
+    for N in $(seq ${REPEAT})
+    do
+        for TASKS in 32 64 80 128 160 256
+        do
+            OUTPUT="output/"
+            OUTPUT+="HOST=${HOSTNAME}/"
+            OUTPUT+="BENCH=$(basename ${BENCH})-${TARGET}/"
+            OUTPUT+="POWER=${SCALING_GOVERNOR}-${SLEEP_STATE}/"
+            OUTPUT+="MONITORING=$(basename ${MONITORING})/"
+            OUTPUT+="TASKS=${TASKS}/"
+	    OUTPUT+="KERNEL=${KERNEL_LOCALVERSION}/"
+	    OUTPUT+="IPANEMA_MODULE=${IPANEMA_MODULE}/"
+	    OUTPUT+="${N}"
+            run_bench
+        done
+    done
+done
